d36534df3ac7770e8d1f6c0f37b0a1475e800056
Jenna Lovett <jennablair12@gmail.com>
2018-07-09 13:13:41 -0400

    Add files via upload

--- export.js
+++ export.js
@@ -1,6 +1,9 @@
 var row_index;
 var col_index;
 var statuschange;
+var bank_links = [];
+var contract_links = [];
+var sa_links = [];
 $(function() {
     $('#load_data').click(function() {
         document.getElementById('submit').innerHTML = '<button type="submit" name="submit_changes" id="submit_changes" class="btn btn-info">Submit Changes</button>';
@@ -9,7 +12,6 @@ $(function() {
         ).click(function(){
             exportTableToCSV($('.mon option:selected').val() + $('.year option:selected').val() + '.csv');
         });
-     //    document.getElementById('write_to_csv').innerHTML = '';
         reloadDOM();
          $.ajax({
              url: $('.mon option:selected').val() + $('.year option:selected').val() + '.csv',
@@ -21,26 +23,35 @@ $(function() {
                      var cell_data = asr_data[count].split(",");
                      table_data += '<tr>';
                      for(var cell_count = 0; cell_count < cell_data.length; cell_count++) {
-                         if(count === 0) {
-                             if(cell_data[cell_count] === 'Previous Status' || cell_data[cell_count] === 'Changed On' || cell_data[cell_count] === 'Status Change?') {
-                                 //do nothing
-                             } else {
-                                table_data += '<th>' + cell_data[cell_count] + '</th>';
-                             }
-                         } else {
-                             if(cell_data[cell_count] === '---') {
-                                 //do nothing
-                             } else {
+                         if(count === 0 && cell_count < 9) {
+                            table_data += '<th>' + cell_data[cell_count] + '</th>';
+                        } else {    
+                            if (cell_data[cell_count] === '' && cell_count === 8) {
+                                //do nothing, for textarea space in td
+                            } else if (cell_count === 9) {
+                                //store links for bank name (link to first column)
+                                bank_links.push(cell_data[cell_count]);
+                            } else if (cell_count === 10) {
+                                //store links for contract (link to second column)
+                                contract_links.push(cell_data[cell_count]);
+                            } else if(cell_count === 11) {
+                                //store links for strategic advisor (link to fourth column)
+                                sa_links.push(cell_data[cell_count]);
+                            } else if(cell_count === 6) {
+                                table_data += '<td class="previous_status">' + cell_data[cell_count] + '</td>';
+                            } else if(cell_count === 7) {
+                                table_data += '<td class="status_changed">' + cell_data[cell_count] + '</td>';
+                            } 
+                            else {
                                 table_data += '<td>' + cell_data[cell_count] + '</td>';
-                             }
-                            
+                            }     
                          }
                      }
                      if(count === 0) {
-                         table_data += '<th>Previous Status</th><th>Changed On</th><th>Status Change?</th></tr>';
+                         // do nothing
                      } else {
-                         table_data += '<td class="previous_status">   ---   </td><td class="status_changed">   ---   </td><td><textarea name="status_change" class="status_change_input" maxlength="50" style="resize:none"/></td></tr>';
-                     }
+                         table_data += '<td><textarea name="status_change" class="status_change_input" maxlength="50" style="resize:none"/></td></tr>';
+                    }
                  }
                  table_data += '</table>';
                  $('#asr_table').html(table_data);       //send data to table data to html
